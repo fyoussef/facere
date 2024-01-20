5d159b8a443f576947f34e6083c6674d37fbcdeb
fyoussef <fillipyuou@gmail.com>
2024-01-20 09:20:58 -0300

    implement templates to read typescript files with calss and interface

--- src/facere.rs
+++ src/facere.rs
@@ -1,9 +1,47 @@
 use crate::{utils, recipient::Recipient};
 
-use std::fs;
+use std::{fs, collections::HashMap, process};
 
-pub fn facere(recipient: &Recipient) {
+#[derive(Hash, Eq, PartialEq, Debug)]
+pub struct Template {
+    pub name: String,
+    pub short: String
+}
+
+impl Template {
+    fn new(name: String, short: String) -> Template {
+        Template { name, short }
+    }
+
+    pub fn get_template_content(name: String) -> String {
+        let templs = HashMap::from([
+            (
+                Template::new("class".to_string(), "cl".to_string()), 
+                "export class ClassName {}".to_string()
+            ),
+            (
+                Template::new("interface".to_string(), "itf".to_string()), 
+                "export interface IInterfaceName {}".to_string()
+            )
+        ]);
+        let mut result = String::new();
+        for (templ, content) in templs {
+            if templ.name.eq(&name.to_lowercase()) || templ.short.eq(&name) {
+                result = content;
+                break;
+            }
+        }
+        if result.is_empty() {
+            eprintln!("Invalid template provided");
+            process::exit(1);
+        }
+        result
+    }
+}
+
+pub fn facere(recipient: &Recipient, template_name: &String) {
     utils::create_dirs(&recipient.path);
     let path = format!("{}/{}", &recipient.path, &recipient.filename);
-    fs::write(path, "").unwrap();
+    let content = Template::get_template_content(template_name.to_owned());
+    fs::write(path, content).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,35 @@ mod recipient;
 mod facere;
 
 use recipient::Recipient;
-use std::{
-    env,
-    process,
-};
+use std::process;
+use clap::Parser;
 
-#[derive(Debug)]
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 struct Input {
-    template: String,
-    path: String,
+    template: Option<String>,
+    path: Option<String>,
 }
 
 fn main() {
-    let template = env::args().nth(1).unwrap_or_else(|| {
+    let input = Input::parse();
+    let template = input.template.unwrap_or_else(|| {
         eprintln!("Invalid template provided");
         process::exit(1);
     });
-    let path = env::args().nth(2).unwrap_or_else(|| {
+    let path = input.path.unwrap_or_else(|| {
         eprintln!("Invalid path provided");
         process::exit(1);
     });
-    let input = Input { template, path };
-    let recipient = Recipient::new(&input.path);
+    let recipient = Recipient::new(&path);
     let file_exists = utils::file_exists(&recipient.full_path);
     if file_exists {
         let can_continue = utils::can_continue();
         if can_continue {
-            facere::facere(&recipient);
+            facere::facere(&recipient, &template);
         } else {
             process::exit(1);
         }
     }
-    facere::facere(&recipient);
+    facere::facere(&recipient, &template);
 }
